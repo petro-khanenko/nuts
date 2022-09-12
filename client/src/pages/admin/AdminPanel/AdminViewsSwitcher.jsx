6fe4ca3c3ba36244afa3ceac8a641ce57c9ba874
Petro Khanenko <75987847+petro-khanenko@users.noreply.github.com>
2022-09-12 23:00:32 +0300

    change views buttons

--- client/src/pages/admin/AdminPanel/AdminViewsSwitcher.jsx
+++ client/src/pages/admin/AdminPanel/AdminViewsSwitcher.jsx
@@ -1,26 +1,36 @@
 import React from 'react'
+import {Button, makeStyles} from "@material-ui/core";
 import {adminViews} from "../../../constants/constants";
-import {Button} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+        viewButton: {
+            minWidth: '160px',
+            justifySelf: 'center',
+            marginRight: '10px',
+            border: 'solid 3px rgba(33,30,30,0.8)'
+        }
+    })
+)
 
 export const AdminViewsSwitcher = ({switchMode, onSwitchView}) => {
-
+    const {viewButton} = useStyles();
     return (
         <div>
             {
                 Object.values(adminViews).map(view => (
-                    // <Button
-                    //     variant='contained'
-                    //     color='primary'
-                    //     disabled={switchMode === view}
-                    //     onClick={() => onSwitchView(view)}
-                    // >
-                    //     {view}
-                    // </Button>
-                    <button onClick={() => onSwitchView(view)}
-                            disabled={switchMode === view}
+                    <Button
+                        variant='contained'
+                        color={switchMode === view ? 'secondary' : 'primary'}
+                        className={viewButton}
+                        onClick={() => onSwitchView(view)}
                     >
                         {view}
-                    </button>
+                    </Button>
+                    // <button onClick={() => onSwitchView(view)}
+                    //         disabled={switchMode === view}
+                    // >
+                    //     {view}
+                    // </button>
                 ))
             }
         </div>
